ad94688e604251ee142e1cf2d2663958378ffded
Dwi Handoyo <107865228+dwi-handoyo@users.noreply.github.com>
2022-07-05 23:28:08 +0700

    Create Outer-Function-Expression-Inner-Function-Expression.js

---
+++ Outer-Function-Expression-Inner-Function-Expression.js
@@ -0,0 +1,25 @@
+
+// CASE #1: OuterFunction = function expression, InnerFunction = function expression
+
+// In this case the object will return undefined value when created. Case #1 not the proper implementation.
+
+const Student = function() {
+ this.name = "Jack Brown";
+ this.age = 23;
+ console.log(this);
+  this.sayHello = function() {
+  console.log(`Hello, my name is ${this.nama}, and I am ${this.umur} years old.`);
+  console.log("Hai");
+  console.log(this);
+ }
+ return this.sayHello;
+}
+
+let student = new Student();
+student();
+
+// output di console:
+// {name: 'Jack Brown', age: 23}
+// Hello, my name is undefined, and I am undefined years old.
+// Hai
+// Window {window: Window, self: Window, document: document, name: '', location: Location, …}
