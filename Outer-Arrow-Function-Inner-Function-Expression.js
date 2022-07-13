808fd0b32061b06a14414cf2694d6c879b185738
Dwi Handoyo <107865228+dwi-handoyo@users.noreply.github.com>
2022-07-13 07:48:27 +0700

    Update Outer-Arrow-Function-Inner-Function-Expression.js

--- Outer-Arrow-Function-Inner-Function-Expression.js
+++ Outer-Arrow-Function-Inner-Function-Expression.js
@@ -1,27 +1,44 @@
 
 // Case #4: OuterFunction = Arrow Function, InnerFunction = Function Expression
+// Object with one method only
 
-// In this case the object will return nothing when created. This case is not correct way in creating object.
+// In this case the object will return value when created.
 
 const Student = () => {
- //const TimBola = function() {
+ //const Student = function() {
  this.name = "Jammie Stampton";
  this.age = 21;
  console.log(this);
  //this.halo = () => {
-   function hello() {
+   this.hello = function() {
    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old. Nice to meet you.`);
-   console.log(this);
+   //console.log(this);
  }
  //return this.halo;
- return hello;
+ return this.hello;
 }
 
 let student = Student();
 student();
 
 // output di console:
-// Nothing, it is because "this" tidak dikenal pada arrow function.
+// "Hello, my name is Jammie Stampton, I am 21 years old. Nice to meet you."
+
+// An alternative to this:
+
+const Student1 = () => {
+ this.name = "Jammie Stampton";
+ this.age = 21;
+ this.hello = function(){
+   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old. Nice to meet you.`);
+ }
+ return this.hello();
+}
+
+Student1();
+
+// output di console:
+// "Hello, my name is Jammie Stampton, I am 21 years old. Nice to meet you."
 
 
 
